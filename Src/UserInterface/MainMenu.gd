08464035662462828850fcaeaac89b0a4591c8a2
Jiri Vavra <houbovo@gmail.com>
2020-01-10 16:16:25 +0100

    new features

--- Src/UserInterface/MainMenu.gd
+++ Src/UserInterface/MainMenu.gd
@@ -1,14 +1,18 @@
 extends Control
 
 var res_arr = [Vector2(480, 270), Vector2(960, 540), Vector2(1920, 1080)]
+var next_scene_path: = "res://Src/Levels/Level01.tscn"
 
 func _ready() -> void:
-	pass
+	print("main menu")
 
 
 func _input(event):
 	if event.is_action_released("change_window_res"):   # Input.is_action_just_pressed("change_window_res"):
 		change_window_size()
+	if event.is_action_released("ui_shoot"):
+		print("change scene")
+		get_tree().change_scene(next_scene_path)
 
 
 func change_window_size() -> void:
